2baeea6f6305ccc9cff6bbd4364d8ced3006c9eb
kuchichan <pawel.kucharski@hotmail.com>
2019-09-17 14:18:14 +0200

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -7,7 +7,7 @@ import time
 
 class NewVisitorTest(LiveServerTestCase):
     MAX_WAIT = 10
-    
+
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -44,24 +44,27 @@ class NewVisitorTest(LiveServerTestCase):
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
             )
-        
+
         # She types "Buy peacock feathers into a text box (Edith hobby was
         # to tie fly-fishing lures)
         inputbox.send_keys("Buy peacock feathers")
-        
+
         # When she hits the enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list.
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
-        
+
         # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        # enters "Use peacock feathers to make a fly"
+        # (Edith is very methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly.')
         inputbox.send_keys(Keys.ENTER)
         # The page updates again, and now shows both items on her list.
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
-        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly.')
+        self.wait_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly.'
+        )
         # Edith wonders  whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
@@ -69,6 +72,29 @@ class NewVisitorTest(LiveServerTestCase):
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+        # She starts a new list and sees the input is nicely centered
+        # there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
