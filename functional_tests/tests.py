b63fcc0922a7bcfb9d86a2e30970c2d0af83f7b5
kuchichan <pawel.kucharski@hotmail.com>
2019-09-17 19:52:10 +0200

    Use Bootstrap

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,11 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     MAX_WAIT = 10
 
     def setUp(self):
