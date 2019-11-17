91b7db47536ba4e3d495cd406f52568b9eae9bdc
kuchichan <pawel.kucharski@hotmail.com>
2019-11-17 15:43:10 +0100

    hack ALLOWED_HOSTS

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@eky7phh_42y7j2d-jrua^(nqbfs!brn^_sd@xvewfj3(xt7(m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
