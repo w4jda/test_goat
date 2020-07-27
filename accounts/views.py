5fdc14ab0c475ffb7485fba0916ba68962745240
kuchichan <pawel.kucharski@hotmail.com>
2020-07-27 21:53:18 +0200

    "Few new tests (mocks) + basic view for email login"

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+from django.contrib import messages
 
-# Create your views here.
+def send_login_email(request):
+    email = request.POST['email']
+    send_mail(
+        'Your login link for Superlists',
+        'Use this link to log in',
+        'noreply@superlists',
+        [email]
+    )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
+    )
+    return redirect('/')
