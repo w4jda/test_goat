9962136bcb0c44a3b8fab1da659e58b636896a70
kuchichan <pawel.kucharski@hotmail.com>
2020-07-29 23:29:23 +0200

    more tests

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from accounts.models import Token
 from unittest.mock import patch
 
 import accounts.views
@@ -9,6 +10,23 @@ class SendLoginEmailViewTest(TestCase):
         response = self.client.post('/accounts/send_login_email', data={
             'email': 'edith@example.com'})
         self.assertRedirects(response, '/')
+
+    def test_creates_a_token_associated_with_email(self):
+        response = self.client.post('/accounts/send_login_email', data={
+            'email': 'edith@example.com'})
+
+        token = Token.objects.first()
+        self.assertEqual(token.email, 'edith@example.com')
+
+    @patch('accounts.views.send_mail')
+    def test_sends_link_to_login_using_token(self, mock_send_mail):
+        response = self.client.post('/accounts/send_login_email', data={
+            'email': 'edith@example.com'})
+
+        token = Token.objects.first()
+        expected_url = f'http://testserver/accounts/login?token={token.uid}'
+        (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
+        self.assertIn(expected_url, body)
 
     @patch('accounts.views.send_mail')
     def test_sends_emails_to_address_from_post(self, mock_send_mail):
@@ -33,3 +51,9 @@ class SendLoginEmailViewTest(TestCase):
                          "Check your email, we've sent you a link you can use to log in."
                          )
         self.assertEqual(message.tags, "success")
+
+
+class LoginViewTest(TestCase):
+    def test_redirects_to_home_page(self):
+        response = self.client.get('/accounts/login?token=abcd123')
+        self.assertRedirects(response, '/')
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,21 @@
 from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
 from django.contrib import messages
 
+from accounts.models import Token
 def send_login_email(request):
     email = request.POST['email']
+    token = Token.objects.create(email=email)
+
+    url = request.build_absolute_uri(
+        reverse('login') + '?token=' + str(token.uid)
+    )
+    message_body = f"Use this link to log in:\n\n{url}"
+    
     send_mail(
         'Your login link for Superlists',
-        'Use this link to log in',
+        message_body,
         'noreply@superlists',
         [email]
     )
@@ -14,4 +23,7 @@ def send_login_email(request):
         request,
         "Check your email, we've sent you a link you can use to log in."
     )
+    return redirect('/')
+
+def login(request):
     return redirect('/')
