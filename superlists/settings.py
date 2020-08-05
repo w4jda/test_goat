e79a56d0b7077f3816ce27fd38e28b0940bf52ec
kuchichan <pawel.kucharski@hotmail.com>
2020-08-05 20:34:35 +0200

    logging in settings

--- superlists/settings.py
+++ superlists/settings.py
@@ -138,3 +138,22 @@ EMAIL_HOST_USER =  os.environ.get('EMAIL_HOST')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+# LOGGING
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
