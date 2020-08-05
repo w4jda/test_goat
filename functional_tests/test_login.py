5fb70ae2df8e14ad352687c4450e6eae8cbd21c2
kuchichan <pawel.kucharski@hotmail.com>
2020-08-05 21:31:34 +0200

    testing mail with staging server

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,21 +1,59 @@
+import re
+import os
+import time
+import poplib
 from django.core import mail
 from selenium.webdriver.common.keys import Keys
-import re
+
 
 from .base import FunctionalTest
 
-TEST_MAIL = 'edith@example.com'
 SUBJECT = 'Your login link for Superlists'
 
 class LoginTest(FunctionalTest):
+
+    def wait_for_email(self, test_email, subject):
+        if not self.staging_server:
+            email = mail.outbox[0]
+            self.assertIn(test_email, email.to)
+            self.assertEqual(email.subject, subject)
+            return email.body
+
+        email_id = None
+        start = time.time()
+        inbox = poplib.POP3_SSL('poczta.interia.pl')
+        try:
+            inbox.user(test_email)
+            inbox.pass_(os.environ['INTERIA_PASSWORD'])
+            while time.time() - start < 60:
+                count, _ = inbox.stat()
+                for i in reversed(range(max(1, count - 10), count + 1)):
+                    print('getting msg', i)
+                    _, lines, __ = inbox.retr(i)
+                    lines = [l.decode('utf-8') for l in lines]
+                    if f'Subject: {subject}' in lines:
+                        email_id = i
+                        body = '\n'.join(lines)
+                        return body
+                time.sleep(5)
+        finally:
+            if email_id:
+                inbox.dele(email_id)
+                inbox.quit()
+            
+                                  
 
     def test_can_get_email_link_to_log_in(self):
         # Edith goes to the awesome superlists site
         # And notices new function "log in" in the navbar
         # So she enter the email
-
+        if self.staging_server:
+            test_mail = 'webapp.project@interia.pl'
+        else:
+            test_mail = 'edith@example.com'
+        
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_name('email').send_keys(TEST_MAIL)
+        self.browser.find_element_by_name('email').send_keys(test_mail)
         self.browser.find_element_by_name('email').send_keys(Keys.ENTER)
 
         self.wait_for(lambda: self.assertIn(
@@ -24,16 +62,14 @@ class LoginTest(FunctionalTest):
         ))
 
         # She checks her email
-        email = mail.outbox[0]
-        self.assertIn(TEST_MAIL, email.to)
-        self.assertEqual(email.subject, SUBJECT)
+        body = self.wait_for_email(test_mail, SUBJECT)
 
         # It has an unique url in it
-        self.assertIn('Use this link to log in', email.body)
-        url_search = re.search(r'http://.+/.+$', email.body)
+        self.assertIn('Use this link to log in', body)
+        url_search = re.search(r'http://.+/.+$', body)
 
         if not url_search:
-            self.fail(f"Could not find url in email body:\n{email.body}")
+            self.fail(f"Could not find url in email body:\n{body}")
         url = url_search.group(0)
         self.assertIn(self.live_server_url, url)
 
@@ -41,12 +77,12 @@ class LoginTest(FunctionalTest):
         self.browser.get(url)
 
         # She is logged in
-        self.wait_to_be_logged_in(email=TEST_MAIL)
+        self.wait_to_be_logged_in(email=test_mail)
 
         # She is logs out
         self.browser.find_element_by_link_text('Log out').click()
 
         # She is logged out
-        self.wait_to_be_logged_out(email=TEST_MAIL)
+        self.wait_to_be_logged_out(email=test_mail)
 
                       
