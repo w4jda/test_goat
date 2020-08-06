2ffaec2ec5ca77c0900efaadfc81fce72fb0b16c
kuchichan <pawel.kucharski@hotmail.com>
2020-08-06 19:48:32 +0200

    Django commands + fabric API

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -5,6 +5,8 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 
+from .server_tools import reset_database
+
 MAX_WAIT = 10
 
 def wait(func):
@@ -25,6 +27,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.staging_server = os.environ.get('STAGING_SERVER')
         if self.staging_server:
             self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)
 
     def tearDown(self):
         self.browser.quit()
--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -28,7 +28,6 @@ class LoginTest(FunctionalTest):
             while time.time() - start < 60:
                 count, _ = inbox.stat()
                 for i in reversed(range(max(1, count - 10), count + 1)):
-                    print('getting msg', i)
                     _, lines, __ = inbox.retr(i)
                     lines = [l.decode('utf-8') for l in lines]
                     if f'Subject: {subject}' in lines:
